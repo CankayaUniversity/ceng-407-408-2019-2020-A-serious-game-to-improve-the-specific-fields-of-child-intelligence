d5d0b1a6257ed881748972d61bb7d35a3dae7311
Ali Kaan Goksu <47613498+alikaangoksu@users.noreply.github.com>
2020-04-09 16:10:06 +0300

    Add files via upload

---
+++ Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        if (!PuzzleGameControl.youWin)
+        {
+            transform.Rotate(0f, 0f, 90f);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
